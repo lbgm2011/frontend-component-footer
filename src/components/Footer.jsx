7554cc7548ac3af9e6e1b7c0a97d391f7351e88d
Laoska B <lbgm2011@gmail.com>
2024-12-30 22:02:18 +0000

    clear footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -49,24 +49,6 @@ class SiteFooter extends React.Component {
         className="footer d-flex border-top py-3 px-4"
       >
         <div className="container-fluid d-flex">
-          <a
-            className="d-block"
-            href={config.LMS_BASE_URL}
-            aria-label={intl.formatMessage(messages['footer.logo.ariaLabel'])}
-          >
-            <img
-              style={{ maxHeight: 45 }}
-              src={logo || config.LOGO_TRADEMARK_URL}
-              alt={intl.formatMessage(messages['footer.logo.altText'])}
-            />
-          </a>
-          <div className="flex-grow-1" />
-          {showLanguageSelector && (
-            <LanguageSelector
-              options={supportedLanguages}
-              onSubmit={onLanguageSelected}
-            />
-          )}
         </div>
       </footer>
     );
